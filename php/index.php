27d3d46e7a26e22eefaf76f306345be7081663f7
Vodkatypique <clement.caffin@gmail.com>
2020-01-16 14:07:40 +0100

    detourage RoI avec coordonnee des box

--- php/index.php
+++ php/index.php
@@ -1,1 +1,61 @@
 <?php
+    function detourage($tabArg, $pathSource, $largeurCible=NULL, $hauteurCible=NULL)
+    //arg = [$x1, $y1, $x2, $y2]
+    {
+        $extension = image_type_to_extension(getimagesize($pathSource)[2]);
+        try{
+            $im = imagecreatefromjpeg($pathSource);
+        }
+        catch(Exception $e){
+            throw new Exception("Error extension : ".$e, 1);
+        }
+        
+
+        $x1 = PHP_INT_MAX;
+        $y1 = PHP_INT_MAX;
+        $x2 = 0;
+        $y2 = 0;
+
+        foreach ($tabArg as $arguments){
+            $x1 = min($x1, $arguments[0]);
+            $y1 = min($y1, $arguments[1]);
+            $x2 = max($x2, $arguments[2]);
+            $y2 = max($y2, $arguments[3]);
+        }
+
+        $x1 = $x1-($x2-$x1)*0.05;
+        $y1 = $y1-($y2-$y1)*0.05;
+        $width = 1.05*($x2-$x1);
+        $height = 1.05*($y2-$y1);
+
+        if ($largeurCible != NULL) {
+            $width = $largeurCible;
+            $x1 = $x1-intdiv($largeurCible-$width, 2);
+        }
+        if ($hauteurCible != NULL) {
+            $height = $hauteurCible;
+            $y1 = $y1-intdiv($hauteurCible-$height, 2);
+        }
+
+
+        if($x1+$width>imagesx($im)){
+            $x1 = imagesx($im)-$width;
+        }
+        if ($y1+$height>imagesy($im)) {
+            $y1 = imagesy($im)-$height;
+        }
+
+        $x1 = max($x1, 0);
+        $y1 = max($y1, 0);
+        $width = min(imagesx($im), $width);
+        $height = min(imagesy($im), $height);
+        
+        $imCrop = imagecrop($im, ['x' => $x1, 'y' => $y1, 'width' => $width, 'height' => $height]);
+  
+        imagejpeg($imCrop,  "../crop/".explode(".", basename($pathSource))[0]."_crop.jpg");
+        // Libération de la mémoire
+        imagedestroy($im);
+        imagedestroy($imCrop);
+    }
+    detourage(array(array(450, 180, 560, 450)), "../images/image.jpg", NULL, NULL);
+?>
