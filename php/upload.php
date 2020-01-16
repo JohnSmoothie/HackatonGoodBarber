4e1ed149e3ac1d472af5a7b4b157d7894b775622
Bastianu <35289464+Bastianu@users.noreply.github.com>
2020-01-16 15:42:37 +0100

    Update upload.php

--- php/upload.php
+++ php/upload.php
@@ -7,10 +7,10 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+      //  echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+      //  echo "File is not an image.";
         $uploadOk = 0;
     }
 }
@@ -21,12 +21,12 @@ if(isset($_POST["submit"])) {
 }*/
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
+  //  echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    echo "Sorry, only JPG, JPEG, PNG files are allowed.";
+  //  echo "Sorry, only JPG, JPEG, PNG files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
@@ -46,5 +46,5 @@ if ($uploadOk == 0) {
 
 
 
-$imagePath = "image/"+basename( $_FILES["fileToUpload"]["name"]); //path of image
+$imagePath = "../image/"+basename( $_FILES["fileToUpload"]["name"]); //path of image
 ?>
