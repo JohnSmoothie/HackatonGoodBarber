ac8850a1bd9b9c94bdf0e60c144181ca4cd8f056
JohnSmoothie <vicoboix@gmail.com>
2020-01-16 17:40:37 +0100

    ajax

--- script/index.js
+++ script/index.js
@@ -1,44 +1,51 @@
 $(document).ready(function(){
 
-    var nomImage = "../img/sources/objects.jpg";
+    var valider = $("");
 
-    var dict = {};
+    valider.click(function () {
+        var dict = {};
 
-
-    $.ajax({
-        type: 'GET',
-        url: 'php-opencv-examples/detect_objects_by_dnn_mobilenet.php',
-        data: {
-            ImageName: nomImage
-        },
-        success: function (data) {
-            console.log(data);
-            data.each(function (rectangle) {
-                var startX = rectangle.startX;
-                var startY = rectangle.startY;
-                var endX = rectangle.endX;
-                var endY = rectangle.endY;
-                var pourcentage = rectangle.pourcentage;
-
-                list = [startX, startY, endX, endY, pourcentage];
-
-                dict.add(list);
-            });
-
-            console.log(dict);
-        }
-    });
+        var nomImage = "../img/sources/objects.jpg";
 
 
-    $.ajax({
-        type: 'POST',
-        url: 'php/index.php',
-        data: {
-            ImageName: nomImage
-        },
-        success: function (data) {
+        $.ajax({
+            type: 'GET',
+            url: 'php-opencv-examples/detect_objects_by_dnn_mobilenet.php',
+            data: {
+                ImageName: nomImage
+            },
+            success: function (data) {
+                console.log(data);
+                $.each(data, function (i, rectangle) {
+                    var startX = rectangle.startX;
+                    var startY = rectangle.startY;
+                    var endX = rectangle.endX;
+                    var endY = rectangle.endY;
+                    var pourcentage = rectangle.pourcentage;
 
-        }
+                    list = [startX, startY, endX, endY, pourcentage];
+
+                    dict.add(list);
+
+                });
+
+                console.log(dict);
+            }
+        });
+
+
+        $.ajax({
+            type: 'POST',
+            url: 'php/index.php',
+            data: {
+                rectangle: JSON.stringify({dict : dict}),
+
+            },
+            success: function (data) {
+
+            }
+        });
+
     });
 
 });
