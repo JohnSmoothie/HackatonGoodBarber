a99159ff66f4d720b11ebd7817cc360e3c5d57fb
Vodkatypique <clement.caffin@gmail.com>
2020-01-17 09:59:42 +0100

    _

--- php/index.php
+++ php/index.php
@@ -7,14 +7,7 @@
     $hauteur = $_POST["hauteur"];
     $largeur = $_POST["largeur"];
 
-    /*
-    out put  d'index
-{"dict":[[1868.0592041015625,1431.3135738372803,2956.51407623291,2809.6590728759766]]}object(stdClass)#1 (1) { ["dict"]=> array(1) { [0]=> array(4) { [0]=> float(1868.0592041016) [1]=> float(1431.3135738373) [2]=> float(2956.5140762329) [3]=> float(2809.659072876) } } } Resource id #4____x1 org: 9223372036854775807y1 org: 9223372036854775807width org height org x1 : 3532 y1 : 2524 width 500 height 500../img/crop/object6_crop2.jpeg
-
-    */
-    
-    
-    echo $json;
+       
     try{
         $json = json_decode($json);
     }
@@ -42,7 +35,7 @@
             echo "catch";
             throw new Exception("Error extension : ".$e, 1);
         }
-        
+       
         echo "___";
         $x1 = PHP_INT_MAX;
         $y1 = PHP_INT_MAX;
@@ -53,8 +46,10 @@
         $rectangles = json_decode($json, true);
         echo $rectangles;
         echo "_";
+        $rectangles = $rectangles["dict"];
         //print_r($rectangles);
         foreach ($rectangles as $rectangle){
+
             echo "_".$rectangle;
             array_push($tabArg, array($rectangle["x1"], $rectangle["y1"], $rectangle["x2"], $rectangle["y2"]));
         }
@@ -116,5 +111,16 @@
     }
     
     detourage($json, $path, $largeur, $hauteur);
+     /*
+    out put  d'index
+    {$JSON "dict":[[1868.0592041015625,1431.3135738372803,2956.51407623291,2809.6590728759766]]}
+
+    $IM object(stdClass)#1 (1) 
+    
+    { ["dict"]=> array(1) { [0]=> array(4) { [0]=> float(1868.0592041016) [1]=> float(1431.3135738373) [2]=> float(2956.5140762329) [3]=> float(2809.659072876) } } } 
+    Resource id #4____x1 org: 9223372036854775807y1 org: 9223372036854775807
+width org height org x1 : 3532 y1 : 2524 width 500 height 500../img/crop/object6_crop2.jpeg
+
+    */
 
 ?>
